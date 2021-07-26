8f0a0c3330b09fba40cec9572e19c49851744da3
avalladaresm <avalladaresm@unitec.edu>
2021-07-26 05:10:29 -0600

    added EmployeesTable page desktop styling

--- src/components/EmployeesTable.tsx
+++ src/components/EmployeesTable.tsx
@@ -28,6 +28,8 @@ const EmployeesTable = () => {
   const [_isLoading, _setIsLoading] = useState(false);
   const [_oldestAge, _setOldestAge] = useState<number>();
   const [_highestSalary, _setHighestSalary] = useState<number>();
+  const [_currentAgeRange, _setCurrentAgeRange] = useState<[number, number]>();
+  const [_currentSalaryRange, _setCurrentSalaryRange] = useState<[number, number]>();
 
   useEffect(() => {
     let mounted = true;
@@ -49,8 +51,12 @@ const EmployeesTable = () => {
 
   useEffect(() => {
     if (_employees.length > 0) {
-      _setOldestAge(getMaxValue(_employees, "employee_age")?.employee_age ?? 100);
-      _setHighestSalary(getMaxValue(_employees, "employee_salary")?.employee_salary ?? 100);
+      const oldestAge = getMaxValue(_employees, "employee_age")?.employee_age ?? 100;
+      const highestSalary = getMaxValue(_employees, "employee_salary")?.employee_salary ?? 100;
+      _setOldestAge(oldestAge);
+      _setHighestSalary(highestSalary);
+      _setCurrentAgeRange([0, oldestAge]);
+      _setCurrentSalaryRange([0, highestSalary]);
     }
   }, [_employees]);
 
@@ -68,6 +74,7 @@ const EmployeesTable = () => {
       (employee) => employee.employee_age > range[0] && employee.employee_age < range[1]
     );
     _setSearchedEmployees(filteredEmployees);
+    _setCurrentAgeRange(range);
   };
 
   const onSalaryRangeChange = (range: [number, number]) => {
@@ -75,6 +82,7 @@ const EmployeesTable = () => {
       (employee) => employee.employee_salary > range[0] && employee.employee_salary < range[1]
     );
     _setSearchedEmployees(filteredEmployees);
+    _setCurrentSalaryRange(range);
   };
 
   const columns: ColumnsType<Employee> = [
@@ -105,31 +113,93 @@ const EmployeesTable = () => {
   ];
 
   return (
-    <div>
-      <Search placeholder="input search text" onSearch={onSearch} enterButton />
-      <Spin spinning={_isLoading}>
-        {_oldestAge && (
-          <Slider
-            range
-            max={_oldestAge}
-            defaultValue={[0, _oldestAge]}
-            onChange={onAgeRangeChange}
-            tooltipVisible
-          />
-        )}
-      </Spin>
-      <Spin spinning={_isLoading}>
-        {_highestSalary && (
-          <Slider
-            range
-            max={_highestSalary}
-            defaultValue={[0, _highestSalary]}
-            onChange={onSalaryRangeChange}
-            tooltipVisible
-            tipFormatter={(value) => formatCurrency(value)}
-          />
-        )}
-      </Spin>
+    <div style={{ padding: 20, backgroundColor: "#F3F4F6", height: "100vh" }}>
+      <div
+        style={{
+          padding: 10,
+          backgroundColor: "#D1D5DB",
+          borderWidth: 3,
+          borderColor: "#6B7280",
+          borderStyle: "solid",
+          borderRadius: 10,
+          marginBottom: 20
+        }}
+      >
+        <legend style={{ textAlign: "left", fontWeight: 600 }}>Filters</legend>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+            width: "100%",
+            marginBottom: 20
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "30%"
+            }}
+          >
+            <h4 style={{ textAlign: "left" }}>Search employee by name</h4>
+            <Search placeholder="input search text" onSearch={onSearch} enterButton />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "30%"
+            }}
+          >
+            <Spin spinning={_isLoading}>
+              {_oldestAge && (
+                <div>
+                  <h4 style={{ textAlign: "left" }}>Filter employee by range of age</h4>
+                  <Slider
+                    range
+                    max={_oldestAge}
+                    defaultValue={[0, _oldestAge]}
+                    onChange={onAgeRangeChange}
+                  />
+                  <h4 style={{ textAlign: "left" }}>
+                    min: {_currentAgeRange && _currentAgeRange[0]}, max:{" "}
+                    {_currentAgeRange && _currentAgeRange[1]}
+                  </h4>
+                </div>
+              )}
+            </Spin>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "30%"
+            }}
+          >
+            <Spin spinning={_isLoading}>
+              {_highestSalary && (
+                <div>
+                  <h4 style={{ textAlign: "left" }}>Filter employee by range of salary</h4>
+                  <div>
+                    <Slider
+                      range
+                      max={_highestSalary}
+                      defaultValue={[0, _highestSalary]}
+                      onChange={onSalaryRangeChange}
+                      tipFormatter={(value) => formatCurrency(value)}
+                    />
+                  </div>
+                  <h4 style={{ textAlign: "left" }}>
+                    min: {_currentSalaryRange && formatCurrency(_currentSalaryRange[0])}, max:{" "}
+                    {_currentSalaryRange && formatCurrency(_currentSalaryRange[1])}
+                  </h4>
+                </div>
+              )}
+            </Spin>
+          </div>
+        </div>
+      </div>
       <Table
         rowKey="id"
         columns={columns}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,7 +7,7 @@ export const getMaxValue = (data: any, propertyToEvaluate: string) => {
 };
 
 export const formatCurrency = (value: number | undefined) => {
-  if (!value) return "Error displaying value";
+  if (value === null || value === undefined) return "Error displaying value";
   return value.toLocaleString("en-US", {
     style: "currency",
     currency: "USD",
