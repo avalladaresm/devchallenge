6e2f828cda9350db07d35604d5b8e16e46f3044b
avalladaresm <avalladaresm@unitec.edu>
2021-07-27 13:10:14 -0600

    added update, delete employee services

--- src/services/index.ts
+++ src/services/index.ts
@@ -8,3 +8,25 @@ export const FetchEmployees = async () => {
     throw e;
   }
 };
+
+export const UpdateEmployee = async (employeeId: number) => {
+  try {
+    const updateResult = await axios.put(
+      `https://dummy.restapiexample.com/api/v1/update/${employeeId}`
+    );
+    return updateResult?.data;
+  } catch (e) {
+    throw e;
+  }
+};
+
+export const DeleteEmployee = async (employeeId: number) => {
+  try {
+    const deleteResult = await axios.delete(
+      `https://dummy.restapiexample.com/api/v1/delete/${employeeId}`
+    );
+    return deleteResult?.data;
+  } catch (e) {
+    throw e;
+  }
+};
